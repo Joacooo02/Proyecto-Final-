2c3cb525679ecf2e62ac422d685620cdb11e4fcc
Juanse Ibarra <juanseibarra1@gmail.com>
2026-05-04 11:18:57 -0300

    BDD - Area Academica.

--- src/main/resources/BDD-TrabajoFInal.sql
+++ src/main/resources/BDD-TrabajoFInal.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE TrabajoFinal;
 USE TrabajoFinal;
 
+-- USUARIO Y PERFILES
+
 CREATE TABLE Persona (
 	idPersona INT AUTO_INCREMENT PRIMARY KEY,
 	nombre VARCHAR (50),
@@ -15,7 +17,7 @@ CREATE TABLE Alumno (
 	idAlumno INT AUTO_INCREMENT PRIMARY KEY,
     idPersona INT,
     legajo INT,
-    anioIngreso INT,
+    anioIngreso YEAR,
     analiticoParcial BOOLEAN,
     esRegular BOOLEAN,
     planEstudio INT,
@@ -37,16 +39,73 @@ CREATE TABLE Profesor(
     FOREIGN KEY (idPersona) REFERENCES Persona(idPersona) ON DELETE CASCADE
 );
 
+-- AREA ADMINISTRATIVA
+
 CREATE TABLE Cuota(
 	idCuota INT AUTO_INCREMENT PRIMARY KEY,
-    idPersona INT,
+    idAlumno INT,
 	valorCuota INT,
     fechaPago DATE,
     fechaVencimiento DATE,
     conceptoCuota ENUM('Cuota', 'Matricula') NOT NULL DEFAULT 'Cuota',
     estadoCuota ENUM('Pagada', 'Pendiente', 'Vencida') NOT NULL DEFAULT 'Pendiente',
-	FOREIGN KEY (idPersona) REFERENCES Persona(idPersona) ON DELETE CASCADE
+	FOREIGN KEY (idAlumno) REFERENCES Alumno(idAlumno) ON DELETE CASCADE
 );
 
+CREATE TABLE Alumno_Cursa_Carrera (
+	idAlumno INT,
+    idCarrera INT,
+    fecha_inscripcion DATE,
+    PRIMARY KEY (idAlumno, idCarrera),
+    FOREIGN KEY (idAlumno) REFERENCES Alumno(idAlumno),
+    FOREIGN KEY (idCarrera) REFERENCES Carrera(idCarrera)
+);
 
+-- AREA ACADEMICA 
+
+CREATE TABLE Carrera(
+	idCarrera INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR (50),
+    duracion INT,
+    tituloOtorgado VARCHAR (50),
+	modalidadCarrera ENUM ('Presencial', 'Virtual') NOT NULL DEFAULT 'Presencial',
+    planDeEstudio year    
+);
+
+CREATE TABLE Materia(
+	idMateria INT AUTO_INCREMENT PRIMARY KEY,
+	idCarrera INT,
+    nombre VARCHAR(50),
+    cargaHoraria INT,
+    cuatrimestre INT,
+    anioCursado YEAR,
+	FOREIGN KEY (idCarrera) REFERENCES Carrera(idCarrera) ON DELETE CASCADE
+);
+
+CREATE TABLE Examen(
+	idExamen INT AUTO_INCREMENT PRIMARY KEY,
+    idMateria INT,
+    fecha DATE,
+    tiporExamen ENUM ('Parcial', 'Final') NOT NULL DEFAULT 'Parcial',
+	FOREIGN KEY (idMateria) REFERENCES Materia(idMateria) ON DELETE CASCADE
+);
+
+CREATE TABLE Nota (
+	idNota INT AUTO_INCREMENT PRIMARY KEY,
+    idExamen INT,
+    idAlumno int,
+    fechaRegistro DATE,
+	FOREIGN KEY (idExamen) REFERENCES Examen(idExamen) ON DELETE CASCADE,
+	FOREIGN KEY (idAlumno) REFERENCES Alumno(idAlumno) ON DELETE CASCADE
+);
+
+CREATE TABLE Comision(
+	idComision INT AUTO_INCREMENT PRIMARY KEY, 
+    idMateria INT,
+    idProfesor INT,
+    nroComision INT,
+    aula VARCHAR (50),
+	FOREIGN KEY (idMateria) REFERENCES Materia(idMateria) ON DELETE CASCADE,
+	FOREIGN KEY (idProfesor) REFERENCES Profesor(idProfesor) ON DELETE CASCADE
+);
 
