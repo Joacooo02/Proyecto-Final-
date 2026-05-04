e3d1b108b81761cc65acf9ebf7542feec8f073c9
joaquin <joaquinsilva0284@gmail.com>
2026-05-04 11:17:17 -0300

    desarrollo de clases area academica

--- src/main/java/model/entity/areaAcademica/Carrera.java
+++ src/main/java/model/entity/areaAcademica/Carrera.java
@@ -1,4 +1,23 @@
 package model.entity.areaAcademica;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Carrera")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Carrera {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String nombre;
+    private Integer duracion;
+    private String tituloOtorgado;
+    @Column(length = 1000)
+    private Integer planEstudio;
 }
--- src/main/java/model/entity/areaAcademica/Examen.java
+++ src/main/java/model/entity/areaAcademica/Examen.java
@@ -1,4 +1,21 @@
 package model.entity.areaAcademica;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Examen")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Examen {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idExamen;
+    private LocalDateTime fecha;
+
 }
