1de8c46126e508fa29b06a704bbd994095160385
Juanse Ibarra <juanseibarra1@gmail.com>
2026-05-04 10:24:24 -0300

    BDD - Area Administrativa.

--- src/main/resources/BDD-TrabajoFInal.sql
+++ src/main/resources/BDD-TrabajoFInal.sql
@@ -32,8 +32,21 @@ CREATE TABLE Administrador(
 CREATE TABLE Profesor(
 	idProfesor INT AUTO_INCREMENT PRIMARY KEY,
     idPersona INT,
-    horasSemanales,
+    horasSemanales INT,
+    estadoProfesor ENUM('ACTIVO', 'INACTIVO', 'LICENCIA') NOT NULL DEFAULT 'ACTIVO',
     FOREIGN KEY (idPersona) REFERENCES Persona(idPersona) ON DELETE CASCADE
 );
+
+CREATE TABLE Cuota(
+	idCuota INT AUTO_INCREMENT PRIMARY KEY,
+    idPersona INT,
+	valorCuota INT,
+    fechaPago DATE,
+    fechaVencimiento DATE,
+    conceptoCuota ENUM('Cuota', 'Matricula') NOT NULL DEFAULT 'Cuota',
+    estadoCuota ENUM('Pagada', 'Pendiente', 'Vencida') NOT NULL DEFAULT 'Pendiente',
+	FOREIGN KEY (idPersona) REFERENCES Persona(idPersona) ON DELETE CASCADE
+);
+
 
 
