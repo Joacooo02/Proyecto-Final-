a7d6edf300de02c2e7319719077bd86448d4d2eb
Joaquin Silva <joaquinsilva0284@gmail.com>
2026-06-01 13:26:59 -0300

    modificacion de base de datos

--- src/main/java/com/sistema/sistema/services/ProfesorService.java
+++ src/main/java/com/sistema/sistema/services/ProfesorService.java
@@ -48,3 +48,4 @@ public class ProfesorService {
         return profesorRepository.save(profesorExistente);
     }
 }
+
--- src/main/resources/BDD-TrabajoFInal.sql
+++ src/main/resources/BDD-TrabajoFInal.sql
@@ -2,7 +2,6 @@ DROP DATABASE IF EXISTS TrabajoFinal;
 CREATE DATABASE IF NOT EXISTS TrabajoFinal;
 USE TrabajoFinal;
 
--- USUARIO Y PERFILES
 
 CREATE TABLE Persona (
     idPersona BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -17,7 +16,7 @@ CREATE TABLE Persona (
 CREATE TABLE Alumno (
     idPersona BIGINT UNSIGNED PRIMARY KEY,
     legajo BIGINT NOT NULL UNIQUE,
-    anioIngreso INTEGER,
+    anioIngreso INT,
     analiticoParcial BOOLEAN,
     esRegular BOOLEAN,
     planEstudio INT,
@@ -37,75 +36,75 @@ CREATE TABLE Profesor (
     FOREIGN KEY (idPersona) REFERENCES Persona(idPersona) ON DELETE CASCADE
 );
 
+-- AREA ACADEMICA
+
+CREATE TABLE Carrera(
+    idCarrera BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50),
+    duracion INT,
+    tituloOtorgado VARCHAR(50),
+    modalidadCarrera ENUM ('Presencial', 'Virtual') NOT NULL DEFAULT 'Presencial',
+    planDeEstudio INT
+);
+
+CREATE TABLE Materia(
+    idMateria BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    idCarrera BIGINT UNSIGNED NOT NULL,
+    nombre VARCHAR(50),
+    cargaHoraria INT,
+    cuatrimestre INT,
+    anioCursado INT,
+    FOREIGN KEY (idCarrera) REFERENCES Carrera(idCarrera) ON DELETE CASCADE
+);
+
+CREATE TABLE Examen(
+    idExamen BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    idMateria BIGINT UNSIGNED NOT NULL,
+    fecha DATE,
+    tipoExamen ENUM ('Parcial', 'Final') NOT NULL DEFAULT 'Parcial',
+    FOREIGN KEY (idMateria) REFERENCES Materia(idMateria) ON DELETE CASCADE
+);
+
+CREATE TABLE Comision(
+    idComision BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    idMateria BIGINT UNSIGNED,
+    idProfesor BIGINT UNSIGNED,
+    nroComision INT,
+    cantAlumnos INT,
+    aula VARCHAR(50),
+    FOREIGN KEY (idMateria) REFERENCES Materia(idMateria) ON DELETE CASCADE,
+    FOREIGN KEY (idProfesor) REFERENCES Profesor(idPersona) ON DELETE CASCADE
+);
+
 -- AREA ADMINISTRATIVA
 
 CREATE TABLE Cuota(
-	idCuota BIGINT UNSIGNED NULL AUTO_INCREMENT PRIMARY KEY,
+    idCuota BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     idAlumno BIGINT UNSIGNED NOT NULL,
-	valorCuota INT,
+    valorCuota INT,
     fechaPago DATE,
     fechaVencimiento DATE,
     conceptoCuota ENUM('Cuota', 'Matricula') NOT NULL DEFAULT 'Cuota',
     estadoCuota ENUM('Pagada', 'Pendiente', 'Vencida') NOT NULL DEFAULT 'Pendiente',
-	FOREIGN KEY (idAlumno) REFERENCES Alumno(idAlumno) ON DELETE CASCADE
+    FOREIGN KEY (idAlumno) REFERENCES Alumno(idPersona) ON DELETE CASCADE
 );
 
 CREATE TABLE Alumno_Cursa_Carrera (
-	idAlumno BIGINT UNSIGNED NOT NULL,
+    idAlumno BIGINT UNSIGNED NOT NULL,
     idCarrera BIGINT UNSIGNED NOT NULL,
     fecha_inscripcion DATE,
     PRIMARY KEY (idAlumno, idCarrera),
-    FOREIGN KEY (idAlumno) REFERENCES Alumno(idAlumno),
-    FOREIGN KEY (idCarrera) REFERENCES Carrera(idCarrera)
-);
-
--- AREA ACADEMICA
-
-CREATE TABLE Carrera(
-	idCarrera BIGINT UNSIGNED NULL AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR (50),
-    duracion INT,
-    tituloOtorgado VARCHAR (50),
-	modalidadCarrera ENUM ('Presencial', 'Virtual') NOT NULL DEFAULT 'Presencial',
-    planDeEstudio INTEGER
-);
-
-CREATE TABLE Materia(
-	idMateria BIGINT UNSIGNED NULL AUTO_INCREMENT PRIMARY KEY,
-	idCarrera BIGINT UNSIGNED NOT NULL,
-    nombre VARCHAR(50),
-    cargaHoraria INT,
-    cuatrimestre INT,
-    anioCursado INTEGER,
-	FOREIGN KEY (idCarrera) REFERENCES Carrera(idCarrera) ON DELETE CASCADE
-);
-
-CREATE TABLE Examen(
-	idExamen BIGINT UNSIGNED NULL AUTO_INCREMENT PRIMARY KEY,
-    idMateria BIGINT UNSIGNED NOT NULL,
-    fecha DATE,
-    tiporExamen ENUM ('Parcial', 'Final') NOT NULL DEFAULT 'Parcial',
-	FOREIGN KEY (idMateria) REFERENCES Materia(idMateria) ON DELETE CASCADE
+    FOREIGN KEY (idAlumno) REFERENCES Alumno(idPersona) ON DELETE CASCADE,
+    FOREIGN KEY (idCarrera) REFERENCES Carrera(idCarrera) ON DELETE CASCADE
 );
 
 CREATE TABLE Nota (
-	idNota BIGINT UNSIGNED NULL AUTO_INCREMENT PRIMARY KEY,
+    idNota BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     idExamen BIGINT UNSIGNED NOT NULL,
     idAlumno BIGINT UNSIGNED NOT NULL,
     nota INT,
     fechaRegistro DATE,
-	FOREIGN KEY (idExamen) REFERENCES Examen(idExamen) ON DELETE CASCADE,
-	FOREIGN KEY (idAlumno) REFERENCES Alumno(idAlumno) ON DELETE CASCADE
-);
-
-CREATE TABLE Comision(
-	idComision BIGINT UNSIGNED NULL AUTO_INCREMENT PRIMARY KEY,
-    idMateria BIGINT,
-    idProfesor BIGINT,
-    nroComision INT,
-    cantAlumnos INT,
-    aula VARCHAR (50),
-	FOREIGN KEY (idMateria) REFERENCES Materia(idMateria) ON DELETE CASCADE,
-	FOREIGN KEY (idProfesor) REFERENCES Profesor(idProfesor) ON DELETE CASCADE
+    FOREIGN KEY (idExamen) REFERENCES Examen(idExamen) ON DELETE CASCADE,
+    FOREIGN KEY (idAlumno) REFERENCES Alumno(idPersona) ON DELETE CASCADE
 );
 
