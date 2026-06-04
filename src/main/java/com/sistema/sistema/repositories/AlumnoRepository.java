4c65e6f935909a76ece0cd6ee78c5202a9f071b9
federico <federicosola09@gmail.com>
2026-06-04 10:13:21 -0300

    agrego en alumno repository

--- src/main/java/com/sistema/sistema/repositories/AlumnoRepository.java
+++ src/main/java/com/sistema/sistema/repositories/AlumnoRepository.java
@@ -3,8 +3,14 @@ package com.sistema.sistema.repositories;
 import com.sistema.sistema.entities.usuario.Alumno;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AlumnoRepository extends JpaRepository<Alumno,Long>, JpaSpecificationExecutor<Alumno> {
+    @Query("SELECT a FROM Alumno a JOIN a.comisiones c WHERE c.materia.id = :materiaId")
+    List<Alumno> findAlumnosByMateriaId(@Param("materiaId") Long materiaId);
 }
--- src/main/java/com/sistema/sistema/services/ProfesorService.java
+++ src/main/java/com/sistema/sistema/services/ProfesorService.java
@@ -87,5 +87,7 @@ public class ProfesorService {
                 ))
                 .collect(Collectors.toList());
     }
+
+    
 }
 
