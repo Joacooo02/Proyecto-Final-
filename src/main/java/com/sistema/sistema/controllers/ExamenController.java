bae12de057dc79787cd2701828c2e5368e2dbbc8
joaquin <joaquinsilva0284@gmail.com>
2026-06-04 09:55:14 -0300

    termino de arreglar errores en agregar un examen

--- src/main/java/com/sistema/sistema/controllers/ExamenController.java
+++ src/main/java/com/sistema/sistema/controllers/ExamenController.java
@@ -1,11 +1,20 @@
 package com.sistema.sistema.controllers;
 
+import com.sistema.sistema.entities.areaAcademica.Examen;
+import com.sistema.sistema.services.ExamenService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/examenes")
 @RequiredArgsConstructor
 public class ExamenController {
+
+    private final ExamenService examenService;
+
+    @PostMapping("/materia/{idMateria}")
+    public Examen agregarExamen(@PathVariable Long idMateria, @RequestBody Examen examen)
+    {
+        return examenService.agregarExamen(idMateria, examen);
+    }
 }
