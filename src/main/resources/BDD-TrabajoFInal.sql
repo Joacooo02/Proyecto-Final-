adcde763d5fa36052a11c055188230733fb36c35
Joaquin Silva <joaquinsilva0284@gmail.com>
2026-06-14 20:33:44 -0300

    Modifico la base de datos agregando tablas e inserts de correlatividad

--- src/main/resources/BDD-TrabajoFInal.sql
+++ src/main/resources/BDD-TrabajoFInal.sql
@@ -38,115 +38,6 @@ CREATE TABLE Profesor (
 
 -- AREA ACADEMICA
 
--- ---------------------------------------------- ESTO ES LO NUEVO QUE AGREGAMOS ----------------------------------------------
-CREATE TABLE PlanEstudio(
-    idPlanEstudio BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    idCarrera BIGINT UNSIGNED NOT NULL,
-    anioInicio INT NOT NULL,
-
-    FOREIGN KEY (idCarrera)
-        REFERENCES Carrera(idCarrera)
-        ON DELETE CASCADE
-);
-
-ALTER TABLE Materia
-ADD COLUMN idPlanEstudio BIGINT UNSIGNED;
-
-ALTER TABLE Materia
-ADD CONSTRAINT fk_materia_plan
-FOREIGN KEY (idPlanEstudio)
-REFERENCES PlanEstudio(idPlanEstudio);
-
-CREATE TABLE Correlatividad(
-    idCorrelatividad BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-
-    idMateria BIGINT UNSIGNED NOT NULL,
-    idMateriaCorrelativa BIGINT UNSIGNED NOT NULL,
-
-    estadoParaCursar ENUM('CURSADA','APROBADA'),
-    estadoParaRendir ENUM('CURSADA','APROBADA'),
-
-    FOREIGN KEY (idMateria)
-        REFERENCES Materia(idMateria)
-        ON DELETE CASCADE,
-
-    FOREIGN KEY (idMateriaCorrelativa)
-        REFERENCES Materia(idMateria)
-        ON DELETE CASCADE
-);
-
-CREATE TABLE Alumno_Materia(
-    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-
-    idAlumno BIGINT UNSIGNED NOT NULL,
-    idMateria BIGINT UNSIGNED NOT NULL,
-
-    estado ENUM(
-        'PENDIENTE',
-        'CURSANDO',
-        'CURSADA',
-        'APROBADA'
-    ) NOT NULL DEFAULT 'PENDIENTE',
-
-    notaFinal DOUBLE,
-    fechaAprobacion DATE,
-
-    UNIQUE(idAlumno,idMateria),
-
-    FOREIGN KEY (idAlumno)
-        REFERENCES Alumno(idPersona)
-        ON DELETE CASCADE,
-
-    FOREIGN KEY (idMateria)
-        REFERENCES Materia(idMateria)
-        ON DELETE CASCADE
-);
-
-CREATE TABLE ComisionHorario(
-    idComisionHorario BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-
-    idComision BIGINT UNSIGNED NOT NULL,
-
-    diaSemana ENUM(
-        'LUNES',
-        'MARTES',
-        'MIERCOLES',
-        'JUEVES',
-        'VIERNES',
-        'SABADO'
-    ) NOT NULL,
-
-    horaInicio TIME NOT NULL,
-    horaFin TIME NOT NULL,
-
-    FOREIGN KEY (idComision)
-        REFERENCES Comision(idComision)
-        ON DELETE CASCADE
-);
-
-CREATE TABLE PeriodoInscripcion(
-    idPeriodo BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-
-    idCarrera BIGINT UNSIGNED NOT NULL,
-
-    tipo ENUM('CURSADA','FINAL') NOT NULL,
-
-    anioLectivo INT NOT NULL,
-    cuatrimestre INT NOT NULL,
-
-    fechaInicio DATETIME NOT NULL,
-    fechaCierre DATETIME NOT NULL,
-
-    activa BOOLEAN DEFAULT TRUE,
-
-    FOREIGN KEY (idCarrera)
-        REFERENCES Carrera(idCarrera)
-        ON DELETE CASCADE
-);
-
-
--- ---------------------------------------------- ESTO ES LO NUEVO QUE AGREGAMOS ----------------------------------------------
-
 CREATE TABLE Carrera(
     idCarrera BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50),
@@ -275,99 +166,248 @@ CREATE TABLE Alumno_Inscripcion_Examen_Final (
 );
 
 
+-- ---------------------------------------------- ESTO ES LO NUEVO QUE AGREGAMOS ----------------------------------------------
+CREATE TABLE PlanEstudio(
+    idPlanEstudio BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    idCarrera BIGINT UNSIGNED NOT NULL,
+    nombre VARCHAR(100) NOT NULL,
+    anioInicio INT NOT NULL,
+
+    FOREIGN KEY (idCarrera)
+        REFERENCES Carrera(idCarrera)
+        ON DELETE CASCADE
+);
+
+ALTER TABLE Materia
+ADD COLUMN idPlanEstudio BIGINT UNSIGNED;
+
+ALTER TABLE Materia
+ADD CONSTRAINT fk_materia_plan
+FOREIGN KEY (idPlanEstudio)
+REFERENCES PlanEstudio(idPlanEstudio);
+
+CREATE TABLE Correlatividad(
+    idCorrelatividad BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+
+    idMateria BIGINT UNSIGNED NOT NULL,
+    idMateriaCorrelativa BIGINT UNSIGNED NOT NULL,
+
+    estadoParaCursar ENUM('CURSADA','APROBADA'),
+    estadoParaRendir ENUM('CURSADA','APROBADA'),
+
+    FOREIGN KEY (idMateria)
+        REFERENCES Materia(idMateria)
+        ON DELETE CASCADE,
+
+    FOREIGN KEY (idMateriaCorrelativa)
+        REFERENCES Materia(idMateria)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE Alumno_Materia(
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+
+    idAlumno BIGINT UNSIGNED NOT NULL,
+    idMateria BIGINT UNSIGNED NOT NULL,
+
+    estado ENUM(
+        'PENDIENTE',
+        'CURSANDO',
+        'CURSADA',
+        'APROBADA'
+    ) NOT NULL DEFAULT 'PENDIENTE',
+
+    notaFinal DOUBLE,
+    fechaAprobacion DATE,
+
+    UNIQUE(idAlumno,idMateria),
+
+    FOREIGN KEY (idAlumno)
+        REFERENCES Alumno(idPersona)
+        ON DELETE CASCADE,
+
+    FOREIGN KEY (idMateria)
+        REFERENCES Materia(idMateria)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE ComisionHorario(
+    idComisionHorario BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+
+    idComision BIGINT UNSIGNED NOT NULL,
+
+    diaSemana ENUM(
+        'LUNES',
+        'MARTES',
+        'MIERCOLES',
+        'JUEVES',
+        'VIERNES',
+        'SABADO'
+    ) NOT NULL,
+
+    horaInicio TIME NOT NULL,
+    horaFin TIME NOT NULL,
+
+    FOREIGN KEY (idComision)
+        REFERENCES Comision(idComision)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE PeriodoInscripcion(
+    idPeriodo BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+
+    idCarrera BIGINT UNSIGNED NOT NULL,
+
+    tipo ENUM('CURSADA','FINAL') NOT NULL,
+
+    anioLectivo INT NOT NULL,
+    cuatrimestre INT NOT NULL,
+
+    fechaInicio DATETIME NOT NULL,
+    fechaCierre DATETIME NOT NULL,
+
+    activa BOOLEAN DEFAULT TRUE,
+
+    FOREIGN KEY (idCarrera)
+        REFERENCES Carrera(idCarrera)
+        ON DELETE CASCADE
+);
+
+
+-- ---------------------------------------------- ESTO ES LO NUEVO QUE AGREGAMOS ----------------------------------------------
+
+
 -- ------------------------------------------TODO ESTO ES DE PRUEBA ----------------------------------------------------------
 -- CARRERA
-INSERT INTO Carrera (nombre,duracion,tituloOtorgado,modalidadCarrera, planDeEstudio)
+INSERT INTO Carrera
+(nombre,duracion,tituloOtorgado,modalidadCarrera)
 VALUES
-('Analista Programador Universitario',3,'Analista Programador Universitario','PRESENCIAL', 2025);
+('Analista Programador Universitario',3,'Analista Programador Universitario','PRESENCIAL');
+
+-- PLAN DE ESTUDIO
+INSERT INTO PlanEstudio
+(idCarrera,nombre,anioInicio)
+VALUES
+(1,'Plan 2025',2025);
 
 -- PERSONA PROFESOR
-INSERT INTO Persona (nombre,apellido,dni,telefono,fechaNacimiento,email,rolUsuario)
+INSERT INTO Persona
+(nombre,apellido,dni,telefono,fechaNacimiento,email,rolUsuario)
 VALUES
 ('Juan','Perez','30111222','2235551234','1985-05-10','juan.perez@gmail.com','PROFESOR');
 
 -- PROFESOR
-INSERT INTO Profesor (idPersona,horasSemanales,estadoProfesor)
+INSERT INTO Profesor
+(idPersona,horasSemanales,estadoProfesor)
 VALUES
 (1,20,'ACTIVO');
 
 -- MATERIAS
-INSERT INTO Materia (idCarrera,nombre,cargaHoraria,cuatrimestre,anioCursado)
+INSERT INTO Materia
+(idCarrera,nombre,cargaHoraria,cuatrimestre,anioCursado)
 VALUES
-(1, 'Programacion I', 96, 1, 1),
-(1, 'Base de Datos I', 96, 2, 1),
-(1, 'Programacion II', 128, 1, 2);
+(1,'Programacion I',96,1,1),
+(1,'Base de Datos I',96,2,1),
+(1,'Programacion II',128,1,2);
 
 -- COMISIONES
-INSERT INTO Comision (idMateria,idProfesor,nroComision,cantAlumnos,aula)
+INSERT INTO Comision
+(idMateria,idProfesor,nroComision,cantAlumnos,aula)
 VALUES
-(1, 1, 101, 35, 'Aula 1'),
-(2, 1, 102, 30, 'Aula 2'),
-(3, 1, 103, 28, 'Aula 3');
+(1,1,101,35,'Aula 1'),
+(2,1,102,30,'Aula 2'),
+(3,1,103,28,'Aula 3');
 
 -- ALUMNO 1
-INSERT INTO Persona (nombre,apellido,dni,telefono,fechaNacimiento,email,rolUsuario)
+INSERT INTO Persona
+(nombre,apellido,dni,telefono,fechaNacimiento,email,rolUsuario)
 VALUES
 ('Pedro','Gomez','40111222','2234441111','2004-03-15','pedro@gmail.com','ALUMNO');
 
-INSERT INTO Alumno (idPersona,legajo,anioIngreso,analiticoParcial,esRegular,planEstudio,promedio)
+INSERT INTO Alumno
+(idPersona,legajo,anioIngreso,analiticoParcial,esRegular,promedio)
 VALUES
-(2,1001,2025,TRUE,TRUE,2025,8.5);
+(2,1001,2025,TRUE,TRUE,8.5);
 
 -- ALUMNO 2
-INSERT INTO Persona (nombre,apellido,dni,telefono,fechaNacimiento,email,rolUsuario)
+INSERT INTO Persona
+(nombre,apellido,dni,telefono,fechaNacimiento,email,rolUsuario)
 VALUES
 ('Maria','Lopez','40222333','2234442222','2003-08-20','maria@gmail.com','ALUMNO');
 
-INSERT INTO Alumno (idPersona,legajo,anioIngreso,analiticoParcial,esRegular,planEstudio,promedio)
+INSERT INTO Alumno
+(idPersona,legajo,anioIngreso,analiticoParcial,esRegular,promedio)
 VALUES
-(3,1002,2024,TRUE,TRUE,2025,9.1);
+(3,1002,2024,TRUE,TRUE,9.1);
 
 -- INSCRIPCION A CARRERA
-INSERT INTO Alumno_Cursa_Carrera (idAlumno,idCarrera,fecha_inscripcion)
+INSERT INTO Alumno_Cursa_Carrera
+(idAlumno,idCarrera,fecha_inscripcion)
 VALUES
-(2, 1, '2025-03-01'),
-(3, 1, '2024-03-01');
+(2,1,'2025-03-01'),
+(3,1,'2024-03-01');
 
--- EXAMEN
-INSERT INTO Examen (idMateria,fecha,tipoExamen)
+-- EXAMEN PARCIAL
+INSERT INTO Examen
+(idMateria,fecha,tipoExamen)
 VALUES
 (1,'2026-06-20','PARCIAL');
 
--- NOTAS
-INSERT INTO Nota (idExamen,idAlumno,nota,fechaRegistro)
+-- EXAMEN FINAL
+INSERT INTO Examen
+(idMateria,fecha,tipoExamen)
 VALUES
-(1, 2, 8, '2026-06-20'),
-(1, 3, 10, '2026-06-20');
+(1,'2026-07-10','FINAL');
+
+-- NOTAS
+INSERT INTO Nota
+(idExamen,idAlumno,nota,fechaRegistro)
+VALUES
+(1,2,8,'2026-06-20'),
+(1,3,10,'2026-06-20');
 
 -- CUOTAS
-INSERT INTO Cuota (idAlumno,valorCuota,fechaPago,fechaVencimiento,conceptoCuota,estadoCuota)
+INSERT INTO Cuota
+(idAlumno,valorCuota,fechaPago,fechaVencimiento,conceptoCuota,estadoCuota)
 VALUES
-(2, 30000, '2026-06-01', '2026-06-10', 'CUOTA', 'PAGADA'),
-(3, 30000, NULL, '2026-06-10', 'CUOTA', 'PENDIENTE');
+(2,30000,'2026-06-01','2026-06-10','CUOTA','PAGADA'),
+(3,30000,NULL,'2026-06-10','CUOTA','PENDIENTE');
 
--- EXAMEN FINAL
-INSERT INTO Examen (idMateria, fecha, tipoExamen)
-VALUES (1, '2026-07-10', 'FINAL');
-
-Update Comision
-set cantAlumnos = 50
-where idComision = 1;
-
--- Alumno 2 cursa varias materias
-INSERT INTO Alumno_Inscripcion_Materia (idAlumno, idMateria, fecha_inscripcion)
+-- INSCRIPCION A MATERIAS
+INSERT INTO Alumno_Inscripcion_Materia
+(idAlumno,idMateria,fecha_inscripcion)
 VALUES
-(2, 1, '2025-03-10'),
-(2, 2, '2025-03-12'),
-(2, 3, '2025-03-15');
+(2,1,'2025-03-10'),
+(2,2,'2025-03-12'),
+(2,3,'2025-03-15'),
+(3,1,'2024-03-10'),
+(3,3,'2024-03-12');
 
--- Alumno 3 cursa algunas materias
-INSERT INTO Alumno_Inscripcion_Materia (idAlumno, idMateria, fecha_inscripcion)
+-- INSCRIPCION A COMISIONES
+INSERT INTO Alumno_Inscripcion_Comision
+(idAlumno,idComision)
 VALUES
-(3, 1, '2024-03-10'),
-(3, 3, '2024-03-12');
+(2,1),
+(2,2),
+(3,1);
 
+-- INSCRIPCION A FINAL
+INSERT INTO Alumno_Inscripcion_Examen_Final
+(idAlumno,idExamen,fecha_inscripcion)
+VALUES
+(2,2,'2026-07-01'),
+(3,2,'2026-07-01');
 
+-- CORRELATIVIDAD
+INSERT INTO Correlatividad
+(idMateria,idMateriaCorrelativa,estadoParaCursar,estadoParaRendir)
+VALUES
+(3,1,'CURSADA','APROBADA');
+
+-- ACTUALIZAR COMISION
+UPDATE Comision
+SET cantAlumnos = 50
+WHERE idComision = 1;
 
 
 SELECT * FROM Persona;
