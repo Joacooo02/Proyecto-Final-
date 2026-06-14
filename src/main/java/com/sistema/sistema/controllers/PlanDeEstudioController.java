6797b42dd0683f4f4663e1b47519fe1e9ccc9590
federico <federicosola09@gmail.com>
2026-06-14 19:56:00 -0300

    plan de estudios controller

---
+++ src/main/java/com/sistema/sistema/controllers/PlanDeEstudioController.java
@@ -0,0 +1,49 @@
+package com.sistema.sistema.controllers;
+
+import com.sistema.sistema.dto.PlanDeEstudioDTO;
+import com.sistema.sistema.entities.areaAcademica.PlanDeEstudio;
+import com.sistema.sistema.services.PlanDeEstudioService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/planDeEstudio")
+public class PlanDeEstudioController {
+    private final PlanDeEstudioService planDeEstudioService;
+
+    public PlanDeEstudioController(PlanDeEstudioService planDeEstudioService) {
+        this.planDeEstudioService = planDeEstudioService;
+    }
+
+    @PostMapping("/crear")
+    public PlanDeEstudioDTO crear(@PathVariable PlanDeEstudio planDeEstudio) {
+        return planDeEstudioService.crear(planDeEstudio);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminar(@PathVariable Long id) {
+        planDeEstudioService.eliminar(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PlanDeEstudioDTO> modificar(@PathVariable Long id, @RequestBody PlanDeEstudioDTO planDeEstudioDTO) {
+        return planDeEstudioService.modificar(id, planDeEstudioDTO)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/mostrar")
+    public List<PlanDeEstudioDTO> mostrar() {
+        return planDeEstudioService.mostrar();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PlanDeEstudioDTO> buscarPorId(@PathVariable Long id) {
+        return planDeEstudioService.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
