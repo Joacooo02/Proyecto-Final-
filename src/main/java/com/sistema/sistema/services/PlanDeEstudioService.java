86e613f9155787e92a4a421ad7f1829c7c6d9f90
federico <federicosola09@gmail.com>
2026-06-14 19:43:41 -0300

    plan de estudio service

--- src/main/java/com/sistema/sistema/services/PlanDeEstudioService.java
+++ src/main/java/com/sistema/sistema/services/PlanDeEstudioService.java
@@ -1,8 +1,13 @@
 package com.sistema.sistema.services;
 
+import com.sistema.sistema.dto.PlanDeEstudioDTO;
+import com.sistema.sistema.entities.areaAcademica.PlanDeEstudio;
 import com.sistema.sistema.mappers.PlanDeEstudioMapper;
 import com.sistema.sistema.repositories.PlanDeEstudioRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PlanDeEstudioService {
@@ -12,5 +17,33 @@ public class PlanDeEstudioService {
     public PlanDeEstudioService(PlanDeEstudioRepository planDeEstudioRepository, PlanDeEstudioMapper planDeEstudioMapper) {
         this.planDeEstudioRepository = planDeEstudioRepository;
         this.planDeEstudioMapper = planDeEstudioMapper;
+    }
+
+    public PlanDeEstudioDTO crear(PlanDeEstudio planDeEstudio) {
+        return planDeEstudioMapper.toDTO(planDeEstudioRepository.save(planDeEstudio));
+    }
+
+    public void eliminar(Long id) {
+        planDeEstudioRepository.deleteById(id);
+    }
+
+    public Optional<PlanDeEstudioDTO> modificar(Long id, PlanDeEstudioDTO planDeEstudioDTO) {
+        return planDeEstudioRepository.findById(id)
+                .map(planDeEstudio -> {
+                    planDeEstudio.setIdCarrera(planDeEstudioDTO.getIdCarrera());
+                    planDeEstudio.setAnioInicio(planDeEstudioDTO.getAnioInicio());
+                    return planDeEstudioMapper.toDTO(planDeEstudioRepository.save(planDeEstudio));
+                });
+    }
+
+    public List<PlanDeEstudioDTO> mostrar() {
+        return planDeEstudioRepository.findAll().stream()
+                .map(planDeEstudioMapper::toDTO)
+                .toList();
+    }
+
+    public Optional<PlanDeEstudioDTO> buscarPorId(Long id) {
+        return planDeEstudioRepository.findById(id)
+                .map(planDeEstudioMapper::toDTO);
     }
 }
