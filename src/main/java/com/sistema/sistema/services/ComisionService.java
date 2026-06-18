30549a0a6c8b771b9e7c8eb42a0b3ab814f657ec
Joaquin Silva <joaquinsilva0284@gmail.com>
2026-06-17 23:29:46 -0300

    Modifico el metodo modificar de comision service porque tiraba en algunos campos null

--- src/main/java/com/sistema/sistema/services/ComisionService.java
+++ src/main/java/com/sistema/sistema/services/ComisionService.java
@@ -2,9 +2,13 @@ package com.sistema.sistema.services;
 
 import com.sistema.sistema.entities.areaAcademica.Comision;
 import com.sistema.sistema.dto.ComisionDTO;
+import com.sistema.sistema.entities.areaAcademica.Materia;
+import com.sistema.sistema.entities.usuario.Profesor;
 import com.sistema.sistema.exceptions.EntidadNoEncontradaException;
 import com.sistema.sistema.mappers.ComisionMapper;
 import com.sistema.sistema.repositories.ComisionRepository;
+import com.sistema.sistema.repositories.MateriaRepository;
+import com.sistema.sistema.repositories.ProfesorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +21,8 @@ public class ComisionService {
 
     private final ComisionRepository comisionRepository;
     private final ComisionMapper comisionMapper;
+    private final MateriaRepository materiaRepository;
+    private final ProfesorRepository profesorRepository;
 
     public Comision buscarComisionPorId(Long id) {
         return comisionRepository.findById(id)
@@ -45,9 +51,21 @@ public class ComisionService {
         comisionExistente.setNroComision(comisionModificada.getNroComision());
         comisionExistente.setCantAlumnos(comisionModificada.getCantAlumnos());
         comisionExistente.setAula(comisionModificada.getAula());
+        comisionExistente.setCupoMaximo(comisionModificada.getCupoMaximo());
 
-        comisionExistente.setMateria(comisionModificada.getMateria());
-        comisionExistente.setProfesor(comisionModificada.getProfesor());
+        if (comisionModificada.getMateria() != null && comisionModificada.getMateria().getIdMateria() != null) {
+            Materia materiaReal = materiaRepository.findById(comisionModificada.getMateria().getIdMateria())
+                    .orElseThrow(() -> new EntidadNoEncontradaException("Materia no encontrada"));
+            comisionExistente.setMateria(materiaReal);
+        }
+
+        if (comisionModificada.getProfesor() != null && comisionModificada.getProfesor().getIdPersona() != null) {
+            Profesor profesorReal = profesorRepository.findById(comisionModificada.getProfesor().getIdPersona())
+                    .orElseThrow(() -> new EntidadNoEncontradaException("Profesor no encontrado"));
+            comisionExistente.setProfesor(profesorReal);
+        } else {
+            comisionExistente.setProfesor(null); // Por si quieren quitar al profesor de la comisión
+        }
 
         return comisionRepository.save(comisionExistente);
     }
