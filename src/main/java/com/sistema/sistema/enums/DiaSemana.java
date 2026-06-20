30068a0dd5d4fb876676cef648869eaaef3bcd52
Joaquin Silva <joaquinsilva0284@gmail.com>
2026-06-20 00:45:12 -0300

    Modifico el enum de DiaSemana cambiando los nombres en mayusculas y poniendo un metodo para que se pasen los dias a mayusculas si viene del fronted

--- src/main/java/com/sistema/sistema/enums/DiaSemana.java
+++ src/main/java/com/sistema/sistema/enums/DiaSemana.java
@@ -1,10 +1,21 @@
 package com.sistema.sistema.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum DiaSemana {
-    Lunes,
-    Martes,
-    Miercoles,
-    Jueves,
-    Viernes,
-    Sabado;
+    LUNES,
+    MARTES,
+    MIERCOLES,
+    JUEVES,
+    VIERNES,
+    SABADO;
+
+    @JsonCreator
+    public static DiaSemana fromString(String value) {
+        if (value == null) return null;
+
+        // Convierte lo que viene del frontend a MAYÚSCULAS
+        // y le quita los espacios para que coincida perfectamente.
+        return DiaSemana.valueOf(value.toUpperCase().trim());
+    }
 }
